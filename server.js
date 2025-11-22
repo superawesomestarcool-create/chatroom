e2f17c81870107b39e372ac9ffa8e1ea205236ab
superawesomestarcool-create <superawesomestarcool@gmail.comL>
2025-11-21 23:37:40 -0500

    Prepare for Render deploy

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ io.on("connection", (socket) => {
     });
 });
 
-http.listen(3001, () => {
-    console.log("Server running on http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+
+http.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
